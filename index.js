d3a2d0905fd15062ae2f693e520426c7a690e3c0
barsu5489 <ebarsulai@gmail.com>
2022-11-21 23:45:03 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(figure)
+{
+  let outcome
+  if (figure <= 400)
+  {
+    outcome = 'This one is on me!';
+  }
+  else if (figure > 2000 && figure <= 2500)
+  {
+    outcome = 'I will gladly take your thirty bucks.';
+  }
+  else if (figure > 2500)
+  {
+    outcome = 'No can do.';
+  }
+  return (outcome);
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city)
+{
+  let destination;
+  destination = city === "NYC" ?  "Ok, sounds good." : "No go.";
+  return (destination);
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip)
+{
+  
+  switch (tip) 
+  {
+    case "generous":
+      return ("Thank you so much.");
+       
+    case "not as generous":
+      return ("Thank you.");
+  
+    default: 
+      return ("Bye.");
+      break;
+  }
 }
